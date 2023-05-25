4de81ae18f63498917953982efbd197b87d919fd
Rabin <rabin.shrestha7@gmail.com>
2023-05-25 15:00:23 -0700

    second commit

--- src/main/java/com/example/jenkins/practise/demo/jenkins/MyController.java
+++ src/main/java/com/example/jenkins/practise/demo/jenkins/MyController.java
@@ -1,10 +1,23 @@
 package com.example.jenkins.practise.demo.jenkins;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MyController {
 
-	public static void main(String[] args) {
-	System.out.println("hiiiiiii");
+	private static final Logger logger = LoggerFactory.getLogger(MyController.class);
+	
+	
+	
+	@GetMapping("/rabin")
+	public String what() {
+		logger.info("whattttt");
+		return "I am fine";
+	}
 
 	}
 
-}
+
